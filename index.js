fe7bb811f6fa56d0743915d07092f18cb695bdd7
marcussandes <marcus.sandesf@gmail.com>
2024-10-08 10:56:47 -0300

    feat: código atualizado

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
-/*# 3️⃣ Escrevendo as classes de um Jogo
 
-**O Que deve ser utilizado**
-
-- Variáveis
-- Operadores
-- Laços de repetição
-- Estruturas de decisões
-- Funções
-- Classes e Objetos
-
-## Objetivo:
-
-Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
-
-- nome
-- idade
-- tipo (ex: guerreiro, mago, monge, ninja )
-
-além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
-
-- exibir a mensagem: "o {tipo} atacou usando {ataque}")
-- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
-- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
-
-se mago -> no ataque exibir (usou magia)
-se guerreiro -> no ataque exibir (usou espada)
-se monge -> no ataque exibir (usou artes marciais)
-se ninja -> no ataque exibir (usou shuriken)
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-
-- "o {tipo} atacou usando {ataque}"
-  ex: mago atacou usando magia
-  guerreiro atacou usando espada
-  */
-
-  class heroi{
-    constructor(nomeHeroi, idadeHeroi, tipoHeroi){
-        this.nomeHeroi = nomeHeroi
-        this.idadeHeroi = idadeHeroi
-        this.tipoHeroi = tipoHeroi
+// Classe Heroi para representar um herói de uma aventura
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
     }
-  }
 
-  let personagem = new heroi("Marcus", 30, "Mago")
-  console.log(personagem)
+    // Método atacar que exibe a mensagem de ataque conforme o tipo de herói
+    atacar() {
+        let ataque;
 
-    
+        // Verifica o tipo de herói e define a mensagem com o ataque apropriado
+        switch (this.tipo.toLowerCase()) {
+            case 'mago':
+                ataque = 'usou magia';
+                break;
+            case 'guerreiro':
+                ataque = 'usou espada';
+                break;
+            case 'monge':
+                ataque = 'usou artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'usou shuriken';
+                break;
+            default:
+                ataque = 'usou um ataque desconhecido';
+                break;
+        }
 
+        // Exibe a mensagem do ataque
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+// Exemplos de criação de objetos Heroi e chamada do método atacar
+const heroi1 = new Heroi('Gandalf', 1000, 'mago');
+const heroi2 = new Heroi('Conan', 35, 'guerreiro');
+const heroi3 = new Heroi('Bruce', 40, 'monge');
+const heroi4 = new Heroi('Naruto', 18, 'ninja');
+
+// Chama o método atacar para cada herói
+heroi1.atacar(); // Saída: O mago atacou usando magia
+heroi2.atacar(); // Saída: O guerreiro atacou usando espada
+heroi3.atacar(); // Saída: O monge atacou usando artes marciais
+heroi4.atacar(); // Saída: O ninja atacou usando shuriken
+
+  
+
+
+  
